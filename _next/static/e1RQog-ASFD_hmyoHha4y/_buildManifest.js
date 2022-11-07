fb9a6ea704b9ef5cadfda2eb394176ff61380cb5
fakhrulnugroho <fakhrulnugroho15@gmail.com>
2022-11-07 05:35:40 +0000

    :rocket: Deploy

---
+++ _next/static/e1RQog-ASFD_hmyoHha4y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-28a8f2f07d28a266.js","static/chunks/pages/index-62cc24225dcd46a4.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
