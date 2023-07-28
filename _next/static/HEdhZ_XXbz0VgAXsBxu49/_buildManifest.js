98991aaad4686c2e6285fbf55f5c642d1cb6d7d5
fakhrulnugroho <fakhrulnugroho15@gmail.com>
2023-07-28 08:36:16 +0000

    :rocket: Deploy

---
+++ _next/static/HEdhZ_XXbz0VgAXsBxu49/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-28a8f2f07d28a266.js","static/chunks/pages/index-ac801dac188cc8b4.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
